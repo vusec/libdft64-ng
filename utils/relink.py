b32c7d75b0d74da835cff8883f919fbda095190e
Cristiano Giuffrida <giuffrida@cs.vu.nl>
2021-07-27 10:51:59 +0200

    relink.py: Improve error handling and use python3.

--- utils/relink.py
+++ utils/relink.py
@@ -1,13 +1,19 @@
-#!/usr/bin/python2
+#!/usr/bin/python3
 import os
 import sys
 import shutil
 
 binary = sys.argv[1]
 addr = 0x7fff00101000 #BIN_START
-os.system("prelink -r" + str(addr) + " " + binary)
+prelink = "prelink"
+prelinkbin = shutil.which(prelink)
+if not prelinkbin:
+    sys.exit("Error: " + prelink + " not found in PATH. Please install it.")
+ret = os.system(prelinkbin + " -r" + str(addr) + " " + binary)
+if ret != 0:
+    sys.exit("Error: Prelinking the binary failed.")
 
 din = open(binary, "rb").read()
-din = din[:0x10] + '\x02' + din[0x11:]
+din = din[:0x10] + b'\x02' + din[0x11:]
 fout = open(binary, "wb")
 fout.write(din)
