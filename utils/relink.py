42487b20b05dc4b51a9ad1f9b3351b7a662ba51e
Brian Johannesmeyer <bjohannesmeyer@gmail.com>
2023-02-07 14:00:34 +0100

    relink: Only relinks if binary is a PIE executable. Adds a message if it does relink.

--- utils/relink.py
+++ utils/relink.py
@@ -2,13 +2,23 @@
 import os
 import sys
 import shutil
+import subprocess
 
 binary = sys.argv[1]
+
+bashbin = shutil.which("bash")
+filebin = shutil.which("file")
+num_pies_cmd = bashbin + " -c \"" + filebin + " " + binary + " | grep pie | wc -l\""
+num_pies = int(subprocess.check_output(num_pies_cmd, shell=True).decode("utf-8"), 10)
+if num_pies == 0:
+    sys.exit("Error: It looks like " + binary + " is not a PIE executable. This is either because it was not built as one, or it was, but has already been relinked.")
+
 addr = 0x7fff00101000 #BIN_START
 prelink = "prelink"
 prelinkbin = shutil.which(prelink)
 if not prelinkbin:
     sys.exit("Error: " + prelink + " not found in PATH. Please install it.")
+print("[libdft] Relinking " + binary)
 ret = os.system(prelinkbin + " -r" + str(addr) + " " + binary)
 if ret != 0:
     sys.exit("Error: Prelinking the binary failed.")
